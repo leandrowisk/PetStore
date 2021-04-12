aa78ce7d8402cff82a17cebd2d350566140a325a
Leandro Tecioni <leandrotdrumond@gmail.com>
2021-04-12 15:25:10 -0300

    1-Criando-o-projeto-e-incluindo-o-menu-principal

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent, MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
+import { MockComponents } from 'ng-mocks';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { MatMenuModule } from '@angular/material/menu';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
+        
       ],
       declarations: [
-        AppComponent
+        
+        MockComponents(
+        AppComponent,
+        MatToolbar,
+        MatSidenav,
+        MatSidenavContent,
+        MatSidenavContainer,
+        MatIcon
+        
+        )
       ],
+      
     }).compileComponents();
   });
 
@@ -20,16 +39,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PetStore'`, () => {
+  it(`should have as title 'Petstore'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PetStore');
+    expect(app.title).toEqual('Petstore');
   });
 
-  it('should render title', () => {
+  it('should render title in mat-toolbar', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('PetStore app is running!');
+    expect(compiled.querySelector('mat-sidenav-content span').textContent).toContain('Petstore');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatSidenavModule, MatSidenavContainer,MatSidenav, MatSidenavContent } from '@angular/material/sidenav';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatToolbarModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
